ab226acee6b706fe3ef094b5b36e07b82bc7f26e
LABC705-25 <LABC705-25@EXAM.UMN.AC.ID>
2019-09-13 14:24:34 +0700

    ini week 4

---
+++ Week04_00000014429/src/app/places/offers/offers.page.ts
@@ -0,0 +1,19 @@
+import { Place } from './../place.model';
+import { Component, OnInit } from '@angular/core';
+import { PlacesService } from '../places.service';
+
+@Component({
+  selector: 'app-offers',
+  templateUrl: './offers.page.html',
+  styleUrls: ['./offers.page.scss'],
+})
+export class OffersPage implements OnInit {
+  loadedPlaces: Place[];
+
+  constructor(private placesService: PlacesService) { }
+
+  ngOnInit() {
+    this.loadedPlaces = this.placesService.places;
+  }
+
+}
